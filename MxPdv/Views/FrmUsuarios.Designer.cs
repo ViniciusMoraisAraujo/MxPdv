38bc942d9cf73210a2bb4a6d9849e68e6a2c0843
Lonezin <vinidev.morais@gmail.com>
2026-03-20 20:00:58 -0300

    Feat: cria frm usuario para criação de novos usuarios

---
+++ MxPdv/Views/FrmUsuarios.Designer.cs
@@ -0,0 +1,145 @@
+using System.ComponentModel;
+
+namespace MxPdv.Views
+{
+    partial class FrmUsuarios
+    {
+        private IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(20, 20);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(36, 13);
+            this.lblLogin.TabIndex = 8;
+            this.lblLogin.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(23, 36);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(350, 20);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(386, 20);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(41, 13);
+            this.lblSenha.TabIndex = 9;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(389, 36);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(200, 20);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(23, 70);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(100, 30);
+            this.btnNovo.TabIndex = 3;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(133, 70);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(100, 30);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(243, 70);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 30);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.AllowUserToAddRows = false;
+            this.dgvUsuarios.AllowUserToDeleteRows = false;
+            this.dgvUsuarios.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(23, 120);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.ReadOnly = true;
+            this.dgvUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsuarios.Size = new System.Drawing.Size(740, 300);
+            this.dgvUsuarios.TabIndex = 6;
+            this.dgvUsuarios.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsuarios_CellDoubleClick);
+            // 
+            // FrmUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 441);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblLogin);
+            this.Name = "FrmUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Usuários";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+    }
+}
---
+++ MxPdv/Views/FrmUsuarios.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using MxPdv.Data;
+using MxPdv.Entities;
+
+namespace MxPdv.Views
+{
+    public partial class FrmUsuarios : Form
+    {
+        private int _usuarioIdSelecionado = 0;
+
+        public FrmUsuarios()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+
+        private void CarregarGrid()
+        {
+            try
+            {
+                using (var context = new MxPdvContext())
+                {
+                    var listaDeUsuarios = context.Usuarios
+                        .Select(u => new 
+                        { 
+                            Id = u.Id, 
+                            Login = u.Login 
+                        })
+                        .ToList();
+
+                    dgvUsuarios.DataSource = listaDeUsuarios;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar os utilizadores: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtLogin.Text) || string.IsNullOrWhiteSpace(txtSenha.Text))
+            {
+                MessageBox.Show("O Login e a Senha são obrigatórios!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var context = new MxPdvContext())
+                {
+                    bool loginJaExiste = context.Usuarios
+                        .Any(u => u.Login.ToLower() == txtLogin.Text.ToLower() && u.Id != _usuarioIdSelecionado);
+
+                    if (loginJaExiste)
+                    {
+                        MessageBox.Show("Já existe um utilizador com este Login. Escolha outro.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (_usuarioIdSelecionado == 0) // MODO INCLUSÃO
+                    {
+                        var novoUsuario = new Usuario 
+                        { 
+                            Login = txtLogin.Text,
+                            Senha = txtSenha.Text
+                        };
+                        context.Usuarios.Add(novoUsuario);
+                    }
+                    else 
+                    {
+                        var usuarioExistente = context.Usuarios.Find(_usuarioIdSelecionado);
+                        if (usuarioExistente != null)
+                        {
+                            usuarioExistente.Login = txtLogin.Text;
+                            usuarioExistente.Senha = txtSenha.Text;
+                        }
+                    }
+
+                    context.SaveChanges();
+                    MessageBox.Show("Utilizador salvo com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                    LimparCampos();
+                    CarregarGrid();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (_usuarioIdSelecionado == 0)
+            {
+                MessageBox.Show("Selecione um utilizador na lista para excluir.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var context = new MxPdvContext())
+            {
+                if (context.Usuarios.Count() <= 1)
+                {
+                    MessageBox.Show("Não pode excluir o único utilizador do sistema, senão nunca mais consegue fazer Login!", "Segurança", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+            }
+
+            if (MessageBox.Show("Tem certeza que deseja excluir este utilizador?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    using (var context = new MxPdvContext())
+                    {
+                        var usuario = context.Usuarios.Find(_usuarioIdSelecionado);
+                        if (usuario != null)
+                        {
+                            context.Usuarios.Remove(usuario);
+                            context.SaveChanges();
+                            MessageBox.Show("Utilizador excluído!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            LimparCampos();
+                            CarregarGrid();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao excluir: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void dgvUsuarios_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow linha = dgvUsuarios.Rows[e.RowIndex];
+                
+                _usuarioIdSelecionado = Convert.ToInt32(linha.Cells["Id"].Value);
+                txtLogin.Text = linha.Cells["Login"].Value.ToString();
+                
+                using (var context = new MxPdvContext())
+                {
+                    var usuario = context.Usuarios.Find(_usuarioIdSelecionado);
+                    if (usuario != null)
+                    {
+                        txtSenha.Text = usuario.Senha;
+                    }
+                }
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtLogin.Clear();
+            txtSenha.Clear();
+            _usuarioIdSelecionado = 0;
+            txtLogin.Focus();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+            {
+                SendKeys.Send("{TAB}");
+                return true; 
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
